5a45f6dad7c3cda81d023408ef0430f57cbec5cf
Dmitrii <dminokin@gmail.com>
2024-04-29 00:39:29 +0300

    default font fix

--- drawer.py
+++ drawer.py
@@ -7,8 +7,8 @@ def draw_text(template_path, text, save_path):
     draw = ImageDraw.Draw(template)
 
     # text
-    font = ImageFont.truetype("arial.ttf", 40)
-    x, y = 350, 26  # random.randint(100, 600), random.randint(100, 700)
+    font = ImageFont.load_default(size=40)
+    x, y = 350, 40  # random.randint(100, 600), random.randint(100, 700)
     position = (x, y)
     text_color = (255, 250, 250)
     draw.text(position, text, fill=text_color, font=font)
@@ -23,3 +23,7 @@ def generate_avatar():
     text = now.strftime("%A\n%B %d, %Y\n%H:%M")
     print(f'generate_avatar {text = }')
     draw_text(template_path='avatar.jpg', text=text, save_path='avatar_time.jpg')
+
+
+if __name__ == '__main__':
+    generate_avatar()
