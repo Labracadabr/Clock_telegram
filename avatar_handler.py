c7a152af73b2bd70d497c33e79346a2eaf30bad7
Dmitrii <dminokin@gmail.com>
2024-10-03 17:03:51 +0800

    diff.total_seconds fix

--- avatar_handler.py
+++ avatar_handler.py
@@ -27,17 +27,19 @@ async def delete_photo():
         last_photo = photos[0]
 
         # чтобы случайно не удалить старые аватарки
-        date = last_photo.date
         now = datetime.now()
-        diff: timedelta = now - date
-        if diff.seconds > 60*60*24:
-            print(f'Нет нового фото. Самое старое загружено {diff.seconds} сек назад: {date = }')
+        diff: timedelta = now - last_photo.date
+        old_sec = 60*60*24
+        if diff.total_seconds() > old_sec:
+            print(f'Нет нового фото. Самое старое загружено {diff.days} дней назад: {last_photo.date = }')
             return False
+        else:
+            print('Удаление недавнего фото')
 
-        # удаление авы
-        result = await app.delete_profile_photos(last_photo.file_id)
-        print(f'{result = }')
-        return result
+            # удаление авы
+            result = await app.delete_profile_photos(last_photo.file_id)
+            print(f'{result = }')
+            return result
 
 
 if __name__ == '__main__':
